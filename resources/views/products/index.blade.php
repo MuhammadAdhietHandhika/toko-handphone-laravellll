3161abbed74985f64afc7bade83af2d2b6015347
muhammadadhiethandhika <muhamadadit845@gamil.com>
2026-06-10 22:45:58 +0700

    update project

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body class="p-5">
-    <h1 class="text-2xl font-bold mb-5">Daftar Produk</h1>
+    @include('navbar')
     <button onclick="toggle_model()" class="bg-blue-500 text-white px-4 py-2 rounded-2xl">+ tambah item</button>
     <table class="table-auto w-full mt-5">
         <thead>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ use Illuminate\Support\Facades\Route;
 //halaman utama langsung login
 Route::get('/',[AuthController::class,'showLogin'])->name('login');
 route::post('/login', [AuthController::class,'Login'])->name('login.process');
-route::resource('products',ProductControllers::class);
+route::post('/logout', [AuthController::class,'logout'])->name('logout');
+
+//protect halaman prdocuts supaya 
+Route::middleware('auth')->group(function(){
+    route::resource('products',ProductControllers::class);
+});
